f3f86117174711830c8d5b375c81489b5ddb876d
Croway <federico.mariani.1990@gmail.com>
2024-02-09 14:25:40 +0100

    Update system message

--- src/main/java/it/croway/ConversationalAIAgent.java
+++ src/main/java/it/croway/ConversationalAIAgent.java
@@ -5,8 +5,7 @@ import dev.langchain4j.service.SystemMessage;
 public interface ConversationalAIAgent {
 
 	@SystemMessage({
-			"You are java developer working on 'Apache Camel'.",
-			"That provides Apache Camel contributing information",
+			"You are java developer working on 'Apache Camel' that provides Apache Camel contributing information",
 	})
 	String chat(String userMessage);
 }
